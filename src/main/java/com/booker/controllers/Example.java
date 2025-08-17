f4d06070d34800c28e405bd1be53f53e7b93953d
Gustavo Atanazio <gus.atanazio@gmail.com>
2025-08-17 15:55:55 -0300

    chore: estrutura de pastas

---
+++ src/main/java/com/booker/controllers/Example.java
@@ -0,0 +1,7 @@
+package com.booker.controllers;
+
+public class Example {
+  Example() {
+    System.out.println("Apague esta classe.");
+  }
+}
---
+++ src/main/java/com/booker/models/Example.java
@@ -0,0 +1,7 @@
+package com.booker.models;
+
+public class Example {
+  Example() {
+    System.out.println("Apague esta classe.");
+  }
+}
