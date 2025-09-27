05db4b487b7f468204ac5687bd83380090064765
Matheus Zanutin <119885089+Mats057@users.noreply.github.com>
2025-09-27 16:12:36 -0300

    tests: mudando as configurações de ambiente para os testes

--- src/test/java/com/booker/BookerApplicationTests.java
+++ src/test/java/com/booker/BookerApplicationTests.java
@@ -2,8 +2,10 @@ package com.booker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BookerApplicationTests {
 
   @Test
