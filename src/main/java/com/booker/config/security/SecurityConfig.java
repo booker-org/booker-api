de6dd23fffaccdb5c74026b903f6112aa7209f4d
Matheus Zanutin <119885089+Mats057@users.noreply.github.com>
2026-01-17 19:49:15 -0300

    add swagger different url to exception and tries to fix ci connection with the container db

--- src/main/java/com/booker/config/security/SecurityConfig.java
+++ src/main/java/com/booker/config/security/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig {
         .requestMatchers(
           "/auth/**",
           "/swagger-ui/**",
+          "/swagger-ui.html",
           "/v3/api-docs/**",
           "/actuator/health")
         .permitAll()
