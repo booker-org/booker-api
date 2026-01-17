d7c6d6577f0f62d2856b89fb812fd208ea42f9b5
Matheus Zanutin <119885089+Mats057@users.noreply.github.com>
2026-01-17 19:19:39 -0300

    fix: import linespace

--- src/main/java/com/booker/mappers/BookMapper.java
+++ src/main/java/com/booker/mappers/BookMapper.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
+
 import com.booker.DTO.Book.BookCreateDTO;
 import com.booker.DTO.Book.BookDTO;
 import com.booker.DTO.Book.BookDetailDTO;
