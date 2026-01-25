1c0b6aeae88267da830b7defc85dfed7457b0a60
Gustavo Atanazio <gus.atanazio@gmail.com>
2026-01-25 17:26:17 -0300

    feat: Review DTOs

---
+++ src/main/java/com/booker/DTO/Review/CreateReviewDTO.java
@@ -0,0 +1,32 @@
+package com.booker.DTO.Review;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+
+public record CreateReviewDTO(
+  @NotNull(message = "Score is required")
+  @PositiveOrZero(message = "Score must be a positive number")
+  @Max(value = 5, message = "Score max value is {max}")
+  @Digits(integer = 1, fraction = 1, message = "Score must have up to {integer} integer digit and {fraction} decimal digit")
+  BigDecimal score,
+
+  @Size(min =  1, max = 50, message = "Headline length must be between {min} and {max}")
+  String headline,
+
+  @NotBlank(message = "Text is required")
+  @Size(max = 2048, message = "Text length must be at most {max}")
+  String text,
+
+  @NotNull(message = "User ID is required")
+  UUID userID,
+
+  @NotNull(message = "Book ID is required")
+  UUID bookID
+) {}
